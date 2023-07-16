f7865ee340dc01df59ed45817c6093c8ba1a655b
harshavardhanakrishnachaitanya2001 <pvageta02@gmail.com>
2023-07-16 11:13:26 +0530

    Don with the testimonials section

--- components/TestimonialsSection.tsx
+++ components/TestimonialsSection.tsx
@@ -35,7 +35,320 @@ const TestimonialsSection = () => {
                     </p>
                   </div>
                 </div>
+                <div className="max-w-[150px] w-full flex items-center sm:justify-end lg:justify-start xl:justify-end mt-4 sm:mt-0 lg:mt-4 xl:mt-0">
+                  <div>
+                    <div className="flex items-center">
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                    </div>
+                    <p className="mt-2 text-sm font-medium text-gray-400">
+                      Client's Review
+                    </p>
+                  </div>
+                </div>
               </div>
+              <p className="text-lg font-medium text-gray-400 leading-relaxed pt-8 mt-6 border-t border-[#eee]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                commodo urna massa, nec dictum velit dignissim ac. Curabitur non
+                cursus tortor. Nunc dignissim accumsan commodo.
+              </p>
+            </div>
+          </div>
+          <div className="w-full lg:w-1/2 px-4">
+            <div className="overflow-hidden rounded-sm p-8 lg:px-6 xl:px-8 mb-10">
+              <div className="sm:flex justify-between lg:block xl:flex">
+                <div className="w-full flex items-center">
+                  <div className="rounded-full w-[60px] h-[60px] overflow-hidden mt-2 mr-4">
+                    <Image src="/author-02.png" alt="" width={60} height={60} />
+                  </div>
+                  <div className="w-full">
+                    <h5 className="text-base md:text-lg lg:text-base xl:text-lg font-medium mb-1">
+                      Devid Miller
+                    </h5>
+                    <p className="text-base font-medium text-gray-400">
+                      Graphic Designer
+                    </p>
+                  </div>
+                </div>
+                <div className="max-w-[150px] w-full flex items-center sm:justify-end lg:justify-start xl:justify-end mt-4 sm:mt-0 lg:mt-4 xl:mt-0">
+                  <div>
+                    <div className="flex items-center">
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                    </div>
+                    <p className="mt-2 text-sm font-medium text-gray-400">
+                      Client's Review
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <p className="text-lg font-medium text-gray-400 leading-relaxed pt-8 mt-6 border-t border-[#eee]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                commodo urna massa, nec dictum velit dignissim ac. Curabitur non
+                cursus tortor. Nunc dignissim accumsan commodo.
+              </p>
+            </div>
+          </div>
+          <div className="w-full lg:w-1/2 px-4">
+            <div className="overflow-hidden rounded-sm p-8 lg:px-6 xl:px-8 mb-10">
+              <div className="sm:flex justify-between lg:block xl:flex">
+                <div className="w-full flex items-center">
+                  <div className="rounded-full w-[60px] h-[60px] overflow-hidden mt-2 mr-4">
+                    <Image src="/author-03.png" alt="" width={60} height={60} />
+                  </div>
+                  <div className="w-full">
+                    <h5 className="text-base md:text-lg lg:text-base xl:text-lg font-medium mb-1">
+                      Jonathon Smith
+                    </h5>
+                    <p className="text-base font-medium text-gray-400">
+                      UI/UX Designer
+                    </p>
+                  </div>
+                </div>
+                <div className="max-w-[150px] w-full flex items-center sm:justify-end lg:justify-start xl:justify-end mt-4 sm:mt-0 lg:mt-4 xl:mt-0">
+                  <div>
+                    <div className="flex items-center">
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                    </div>
+                    <p className="mt-2 text-sm font-medium text-gray-400">
+                      Client's Review
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <p className="text-lg font-medium text-gray-400 leading-relaxed pt-8 mt-6 border-t border-[#eee]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                commodo urna massa, nec dictum velit dignissim ac. Curabitur non
+                cursus tortor. Nunc dignissim accumsan commodo.
+              </p>
+            </div>
+          </div>
+          <div className="w-full lg:w-1/2 px-4">
+            <div className="overflow-hidden rounded-sm p-8 lg:px-6 xl:px-8 mb-10">
+              <div className="sm:flex justify-between lg:block xl:flex">
+                <div className="w-full flex items-center">
+                  <div className="rounded-full w-[60px] h-[60px] overflow-hidden mt-2 mr-4">
+                    <Image src="/author-04.png" alt="" width={60} height={60} />
+                  </div>
+                  <div className="w-full">
+                    <h5 className="text-base md:text-lg lg:text-base xl:text-lg font-medium mb-1">
+                      Jubin Nutial
+                    </h5>
+                    <p className="text-base font-medium text-gray-400">
+                      App Developer
+                    </p>
+                  </div>
+                </div>
+                <div className="max-w-[150px] w-full flex items-center sm:justify-end lg:justify-start xl:justify-end mt-4 sm:mt-0 lg:mt-4 xl:mt-0">
+                  <div>
+                    <div className="flex items-center">
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                      <span className="text-yellow-500 mr-1 block">
+                        <svg
+                          width="18"
+                          height="16"
+                          viewBox="0 0 18 16"
+                          className="fill-current"
+                        >
+                          <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z"></path>
+                        </svg>
+                      </span>
+                    </div>
+                    <p className="mt-2 text-sm font-medium text-gray-400">
+                      Client's Review
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <p className="text-lg font-medium text-gray-400 leading-relaxed pt-8 mt-6 border-t border-[#eee]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                commodo urna massa, nec dictum velit dignissim ac. Curabitur non
+                cursus tortor. Nunc dignissim accumsan commodo.
+              </p>
             </div>
           </div>
         </div>
